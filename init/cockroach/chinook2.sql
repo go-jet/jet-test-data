fdb0cc598d2b534310d2b559ce9a2f75b5507c56
go-jet <zer0.sub@protonmail.com>
2022-05-17 11:04:34 +0200

    Add multi-tenant test data.

--- init/cockroach/chinook2.sql
+++ init/cockroach/chinook2.sql
@@ -58,3 +58,112 @@ INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (9, 'Plays Metallica
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (10, 'Audioslave', 8);
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (11, 'Out Of Exile', 8);
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (12, 'BackBeat Soundtrack', 9);
+
+/* Table prefix multi-tenant */
+
+CREATE TABLE "tenant1.Album"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "prefix_tenant1_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+CREATE TABLE "tenant2.Album"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "prefix_tenant2_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (80, 'In Your Honor [Disc 2]', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (81, 'One By One', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (82, 'The Colour And The Shape', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (83, 'My Way: The Best Of Frank Sinatra [Disc 1]', 85);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (84, 'Roda De Funk', 86);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (85, 'As Can��es de Eu Tu Eles', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (86, 'Quanta Gente Veio Ver (Live)', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (87, 'Quanta Gente Veio ver--B�nus De Carnaval', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (88, 'Faceless', 87);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (89, 'American Idiot', 54);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (90, 'Appetite for Destruction', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (91, 'Use Your Illusion I', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (92, 'Use Your Illusion II', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (93, 'Blue Moods', 89);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (94, 'A Matter of Life and Death', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (95, 'A Real Dead One', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (96, 'A Real Live One', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (97, 'Brave New World', 90);
+
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (152, 'Master Of Puppets', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (153, 'ReLoad', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (154, 'Ride The Lightning', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (155, 'St. Anger', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (156, '...And Justice For All', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (157, 'Miles Ahead', 68);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (158, 'Milton Nascimento Ao Vivo', 42);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (159, 'Minas', 42);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (160, 'Ace Of Spades', 106);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (161, 'Demorou...', 108);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (162, 'Motley Crue Greatest Hits', 109);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (163, 'From The Muddy Banks Of The Wishkah [Live]', 110);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (164, 'Nevermind', 110);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (165, 'Compositores', 111);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (166, 'Olodum', 112);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (167, 'Ac�stico MTV', 113);
+
+
+/* Table postfix multi-tenant */
+
+CREATE TABLE "Album.tenant1"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "postfix_tenant1_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+CREATE TABLE "Album.tenant2"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "postfix_tenant2_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (80, 'In Your Honor [Disc 2]', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (81, 'One By One', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (82, 'The Colour And The Shape', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (83, 'My Way: The Best Of Frank Sinatra [Disc 1]', 85);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (84, 'Roda De Funk', 86);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (85, 'As Can��es de Eu Tu Eles', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (86, 'Quanta Gente Veio Ver (Live)', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (87, 'Quanta Gente Veio ver--B�nus De Carnaval', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (88, 'Faceless', 87);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (89, 'American Idiot', 54);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (90, 'Appetite for Destruction', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (91, 'Use Your Illusion I', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (92, 'Use Your Illusion II', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (93, 'Blue Moods', 89);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (94, 'A Matter of Life and Death', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (95, 'A Real Dead One', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (96, 'A Real Live One', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (97, 'Brave New World', 90);
+
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (152, 'Master Of Puppets', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (153, 'ReLoad', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (154, 'Ride The Lightning', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (155, 'St. Anger', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (156, '...And Justice For All', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (157, 'Miles Ahead', 68);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (158, 'Milton Nascimento Ao Vivo', 42);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (159, 'Minas', 42);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (160, 'Ace Of Spades', 106);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (161, 'Demorou...', 108);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (162, 'Motley Crue Greatest Hits', 109);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (163, 'From The Muddy Banks Of The Wishkah [Live]', 110);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (164, 'Nevermind', 110);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (165, 'Compositores', 111);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (166, 'Olodum', 112);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (167, 'Ac�stico MTV', 113);
--- init/postgres/chinook2.sql
+++ init/postgres/chinook2.sql
@@ -56,3 +56,112 @@ INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (9, N'Plays Metallic
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (10, N'Audioslave', 8);
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (11, N'Out Of Exile', 8);
 INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (12, N'BackBeat Soundtrack', 9);
+
+/* Table prefix multi-tenant */
+
+CREATE TABLE "tenant1.Album"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "prefix_tenant1_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+CREATE TABLE "tenant2.Album"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "prefix_tenant2_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (80, N'In Your Honor [Disc 2]', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (81, N'One By One', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (82, N'The Colour And The Shape', 84);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (83, N'My Way: The Best Of Frank Sinatra [Disc 1]', 85);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (84, N'Roda De Funk', 86);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (85, N'As Can��es de Eu Tu Eles', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (86, N'Quanta Gente Veio Ver (Live)', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (87, N'Quanta Gente Veio ver--B�nus De Carnaval', 27);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (88, N'Faceless', 87);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (89, N'American Idiot', 54);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (90, N'Appetite for Destruction', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (91, N'Use Your Illusion I', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (92, N'Use Your Illusion II', 88);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (93, N'Blue Moods', 89);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (94, N'A Matter of Life and Death', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (95, N'A Real Dead One', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (96, N'A Real Live One', 90);
+INSERT INTO "tenant1.Album" ("AlbumId", "Title", "ArtistId") VALUES (97, N'Brave New World', 90);
+
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (152, N'Master Of Puppets', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (153, N'ReLoad', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (154, N'Ride The Lightning', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (155, N'St. Anger', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (156, N'...And Justice For All', 50);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (157, N'Miles Ahead', 68);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (158, N'Milton Nascimento Ao Vivo', 42);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (159, N'Minas', 42);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (160, N'Ace Of Spades', 106);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (161, N'Demorou...', 108);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (162, N'Motley Crue Greatest Hits', 109);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (163, N'From The Muddy Banks Of The Wishkah [Live]', 110);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (164, N'Nevermind', 110);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (165, N'Compositores', 111);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (166, N'Olodum', 112);
+INSERT INTO "tenant2.Album" ("AlbumId", "Title", "ArtistId") VALUES (167, N'Ac�stico MTV', 113);
+
+
+/* Table postfix multi-tenant */
+
+CREATE TABLE "Album.tenant1"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "postfix_tenant1_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+CREATE TABLE "Album.tenant2"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "postfix_tenant2_PK_Album" PRIMARY KEY  ("AlbumId")
+);
+
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (80, N'In Your Honor [Disc 2]', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (81, N'One By One', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (82, N'The Colour And The Shape', 84);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (83, N'My Way: The Best Of Frank Sinatra [Disc 1]', 85);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (84, N'Roda De Funk', 86);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (85, N'As Can��es de Eu Tu Eles', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (86, N'Quanta Gente Veio Ver (Live)', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (87, N'Quanta Gente Veio ver--B�nus De Carnaval', 27);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (88, N'Faceless', 87);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (89, N'American Idiot', 54);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (90, N'Appetite for Destruction', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (91, N'Use Your Illusion I', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (92, N'Use Your Illusion II', 88);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (93, N'Blue Moods', 89);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (94, N'A Matter of Life and Death', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (95, N'A Real Dead One', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (96, N'A Real Live One', 90);
+INSERT INTO "Album.tenant1" ("AlbumId", "Title", "ArtistId") VALUES (97, N'Brave New World', 90);
+
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (152, N'Master Of Puppets', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (153, N'ReLoad', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (154, N'Ride The Lightning', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (155, N'St. Anger', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (156, N'...And Justice For All', 50);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (157, N'Miles Ahead', 68);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (158, N'Milton Nascimento Ao Vivo', 42);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (159, N'Minas', 42);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (160, N'Ace Of Spades', 106);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (161, N'Demorou...', 108);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (162, N'Motley Crue Greatest Hits', 109);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (163, N'From The Muddy Banks Of The Wishkah [Live]', 110);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (164, N'Nevermind', 110);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (165, N'Compositores', 111);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (166, N'Olodum', 112);
+INSERT INTO "Album.tenant2" ("AlbumId", "Title", "ArtistId") VALUES (167, N'Ac�stico MTV', 113);
