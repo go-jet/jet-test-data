2a3648b4f693996488554053b418dd36a5463675
Cedric L'homme <public@l-homme.com>
2023-01-30 14:53:16 -0500

    Move back to test_sample

--- init/postgres/test_sample.sql
+++ init/postgres/test_sample.sql
@@ -370,13 +370,13 @@ VALUES(0, NULL, '1.11111111111111111111', NULL, '2.22222222222222222222', NULL, 
 DROP TABLE IF EXISTS test_sample.people CASCADE;
 CREATE TABLE test_sample.people
 (
-    people_id        integer primary key,
+    people_id        serial primary key,
     people_name      varchar(15) NOT NULL,
     people_height_cm numeric,
     people_height_in numeric GENERATED ALWAYS AS ((people_height_cm / 2.54)) STORED
 );
 
-INSERT INTO test_sample.people (people_id, people_name, people_height_cm) VALUES
-    (1, 'Alfredo', 170),
-    (2, 'Bob', null),
-    (3, 'Carla', 155);
+INSERT INTO test_sample.people (people_name, people_height_cm) VALUES
+    ('Alfredo', 170),
+    ('Bob', null),
+    ('Carla', 155);
