af215f6747a8516438a8b973fc1d70571aa61f6f
Cedric L'homme <public@l-homme.com>
2023-02-01 11:48:06 -0500

    Fix people IDs for CockroachDB

--- init/cockroach/test_sample.sql
+++ init/cockroach/test_sample.sql
@@ -382,7 +382,7 @@ CREATE TABLE test_sample.people
     people_height_in numeric GENERATED ALWAYS AS ((people_height_cm / 2.54)) STORED
 );
 
-INSERT INTO test_sample.people (people_name, people_height_cm) VALUES
-    ('Alfredo', 170),
-    ('Bob', null),
-    ('Carla', 155);
+INSERT INTO test_sample.people (people_id, people_name, people_height_cm) VALUES
+    (1, 'Alfredo', 170),
+    (2, 'Bob', null),
+    (3, 'Carla', 155);
